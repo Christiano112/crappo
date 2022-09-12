90e15b05531deb47cdb65b3c0024a564fe3fc81f
Enyia Christian <94331754+Christiano112@users.noreply.github.com>
2022-09-12 14:38:51 +0100

    Done with the main section

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -7,13 +7,15 @@ import './styles/nav.scss';
 function Nav() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light">
+            <nav className="navbar navbar-expand-lg navbar-dark border border-0">
                 <div className="container">
                     <a className="navbar-brand" href="#home">
                         <img src={LogoIcon} alt="Crappo logo" />
                     </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler border border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="border border-0">
+                            <svg stroke="#fff" fill="#fff" stroke-width="0" viewBox="0 0 20 20" class="lg:hidden text-3xl text-white cursor-pointer" height="2em" width="2em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                        </span>
                     </button>
                     <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                         <ul className="navbar-nav">
