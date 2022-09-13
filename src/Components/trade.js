c45fb7ecfb5bc3163f5b628132befdb6f314b08e
Enyia Christian <94331754+Christiano112@users.noreply.github.com>
2022-09-13 16:39:12 +0100

    Fixed the margins

--- src/Components/trade.js
+++ src/Components/trade.js
@@ -10,7 +10,7 @@ function Trade() {
                         <h4>Trade Securely And Market The High Growth Cryptocurrencies.</h4>
                     </div>
                     <div className="row trade-content text-center">
-                        <div className="col">
+                        <div className="col trade-col">
                             <img src={require("./images/bitcon.png")} alt="bitcoin" />
                             <h2>Bitcoin <span>BTC</span></h2>
                             <p>Digital currency in which a record of transactions is maintained.</p>
@@ -18,7 +18,7 @@ function Trade() {
                                 <img src={require("./images/Arrow Right.jpg")} alt="arrow" />
                             </div>
                         </div>
-                        <div className="col">
+                        <div className="col trade-col">
                             <img src={require("./images/ethereum.png")} alt="ethereum" />
                             <h2>Ethereum <span>ETH</span></h2>
                             <p>Blockchain technology to create and run decentralized digital applications.</p>
@@ -26,7 +26,7 @@ function Trade() {
                                 <img src={require("./images/Arrow Right.jpg")} alt="arrow" />
                             </div>
                         </div>
-                        <div className="col">
+                        <div className="col trade-col">
                             <img src={require("./images/litecoin.png")} alt="litecoin" />
                             <h2>Litecoin <span>LTC</span></h2>
                             <p>Cryptocurrency that enables instant payments to anyone in the world.</p>
